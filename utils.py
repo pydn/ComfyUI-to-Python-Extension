2c75211aa0080368804401856c0b1c4c19558b72
vairleon <vairleon@outlook.com>
2024-09-04 01:43:28 +0800

    fix: decouple from main file

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def add_extra_model_paths() -> None:
     """
     Parse the optional extra_model_paths.yaml file and add the parsed paths to the sys.path.
     """
-    from main import load_extra_path_config
+    from main_utils import load_extra_path_config
 
     extra_model_paths = find_path("extra_model_paths.yaml")
     
